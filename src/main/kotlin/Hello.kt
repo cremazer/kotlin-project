0f1737594ada075432ebc208488c514cbe5c50f0
Kyuseo Shim <cremazer@gmail.com>
2022-08-18 07:52:13 +0900

    var, val, const 상수 실습

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,3 +1,10 @@
+const val MAX_EXPERIENCE = 5_000
+
 fun main() {
-    println("안녕하세요?")
+    val playerName = "에스트라곤"
+    var experiencePoints = 5
+    experiencePoints += 5
+    println("experiencePoints=$experiencePoints")
+    println("playerName= $playerName")
+    println("MAX_EXPERIENCE= $MAX_EXPERIENCE")
 }
