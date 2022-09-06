610e85099dcc5b03ee4f2ad4540a9e3d9b246fbd
Kyuseo Shim <cremazer@gmail.com>
2022-09-07 07:41:40 +0900

    gitignore 수정 반환 타입으로 함수 타입 사용하기 실습

--- src/main/kotlin/game/SimVillage.kt
+++ src/main/kotlin/game/SimVillage.kt
@@ -1,22 +1,22 @@
 package game
 
 fun main() {
-    runSimulation("럭키", ::printConstructionCost) {playerName, numBuildings ->
+    runSimulation()
+}
+
+fun runSimulation() {
+    val greetingFunction = configureGreetingFunction()
+    println(greetingFunction("럭키"))
+    println(greetingFunction("럭키"))
+}
+
+fun configureGreetingFunction(): (String) -> String {
+    val structureType = "병원"
+    var numBuildings = 5
+    return { playerName: String ->
         val currentYear = 2022
-        println("$numBuildings 채의 건물이 추가됨.")
-        "SimVillage 방문을 환영합니다, $playerName 님! (copyright $currentYear)"
+        numBuildings += 1
+        println("$numBuildings 채의 $structureType 이 추가됨")
+        "SimVillage 방문을 환영합니다, $playerName! (copyright $currentYear)"
     }
-}
-
-inline fun runSimulation(playerName: String,
-                         costPrinter: (Int) -> Unit,
-                         greetingFunction: (String, Int) -> String) {
-    val numBuildings = (1..3).shuffled().last()
-    costPrinter(numBuildings)
-    println(greetingFunction(playerName, numBuildings))
-}
-
-fun printConstructionCost(numBuildings: Int) {
-    val cost = 500
-    println("건축 비용: ${cost * numBuildings}")
 }
